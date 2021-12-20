37d8057a4c3f04fca4ee797c57bbdf1b4519a219
P3ar1baron <v.rozanoff@gmail.com>
2021-12-20 13:30:42 +0200

    Add audience, serverUrl parameters

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,16 @@
-import {domain,clientId} from '../../auth_config.json';
+import {domain,clientId,audience,serverUrl} from '../../auth_config.json';
 
 export const environment = {
   production: false,
   auth: {
     domain: "domain",
     clientId: "clientId",
-    redirectUri: window.location.origin
+    redirectUri: window.location.origin,
+    audience,
+  },
+
+  dev: {
+    serverUrl
   }
 };
 
